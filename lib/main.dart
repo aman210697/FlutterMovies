24b56940542b87f6c0fa8ab17b49d51b48503ac5
Aman <aman210697@gmail.com>
2019-09-21 13:21:36 +0530

    details changes

--- lib/main.dart
+++ lib/main.dart
@@ -130,31 +130,34 @@ class MovieWidget extends StatelessWidget {
       onTap: (){
         Navigator.push(context, MaterialPageRoute(builder: (context) => MovieDetails(movie: movie,)));
       },
-          child: Container(
+          child: Hero(
+            tag: movie.title,
+            child: Container(
         alignment: Alignment.center,
         child: Container(
-          margin: EdgeInsets.only(bottom: 10.0, left: 10.0),
-          height: 180.0,
-          width: 130.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(14.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black87,
-                blurRadius: 5.0,
-                offset: Offset(2.0, 3.0),
-              )
-            ],
-            image: DecorationImage(
-              image: NetworkImage(_imageURL + movie.posterPath),
-              alignment: Alignment.center,
-              fit: BoxFit.cover,
+            margin: EdgeInsets.only(bottom: 10.0, left: 10.0),
+            height: 180.0,
+            width: 130.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(14.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black87,
+                  blurRadius: 5.0,
+                  offset: Offset(2.0, 3.0),
+                )
+              ],
+              image: DecorationImage(
+                image: NetworkImage(_imageURL + movie.posterPath),
+                alignment: Alignment.center,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
 
-          
+
         ),
       ),
+          ),
     );
   }
 }
--- lib/movieDetails.dart
+++ lib/movieDetails.dart
@@ -2,18 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter_movies/movie.dart';
 import 'package:flutter_movies/tv.dart';
 
-
 class MovieDetails extends StatelessWidget {
-final Movie movie;
-final Tv tv;
+  final _imageURL = 'https://image.tmdb.org/t/p/w500';
+  final Movie movie;
+  final Tv tv;
 
-MovieDetails({this.movie, this.tv});
+  MovieDetails({this.movie, this.tv});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(movie==null?tv.name:movie.title),),
+      appBar: AppBar(
+        title: Text(movie == null ? tv.name : movie.title),
+      ),
+      body: Scaffold(
+        body: Hero(
+          tag: movie.title,
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
 
+              image: DecorationImage(
+                image: NetworkImage(_imageURL + movie.posterPath),
+                alignment: Alignment.center,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
